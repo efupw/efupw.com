64ea9fbafc69e62d01743a20453cf7c941ea9091
Mikkel Kjeldsen <commonquail@gmail.com>
2015-02-04 21:32:48 +0100

    Query server status

---
+++ server-status.php
@@ -0,0 +1,19 @@
+<?php
+require 'nwn-server-status/nwn_server_status.php';
+
+$hostname = 'localhost';
+$port = 5121;
+
+$expires = time();
+if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE'])) {
+    $last_checked = strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']);
+    if ($last_checked + 60 > $expires) {
+        header('HTTP/1.1 304 Not Modified');
+        exit;
+    }
+}
+
+header('Content-Type: application/json; charset=utf-8');
+header('Cache-Control: max-age=60, must-revalidate');
+header('Last-Modified: ' . gmdate('D, d M Y H:i:s', $expires) . ' GMT');
+echo json_encode(nwn_server_status($hostname, $port));
